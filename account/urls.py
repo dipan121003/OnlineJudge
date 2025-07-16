8ea176619f0c445db45e46caff28dfbfb280da7b
Your Name <you@example.com>
2025-07-16 21:01:46 +0530

    Initial commit

---
+++ account/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from .views import register_user,login_user
+
+urlpatterns = [
+    path('register/', register_user, name='register-user'),
+    path('login/', login_user, name='login-user'),
+]
